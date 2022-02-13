dae8217b091d6662e71b045aac2862c314e78976
ChiuYuan0214 <rumble88481@gmail.com>
2022-02-13 19:08:19 +0800

    add dummy data

--- src/data/dummy-expenses.ts
+++ src/data/dummy-expenses.ts
@@ -8,11 +8,38 @@ export const DUMMY_EXPENSES_DATA = [
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to referenceWarning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
     2,
-    8
+    1
+  ),
+  new Expense(
+    "Buy a bicycle",
+    4500,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to referenceWarning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    2,
+    2
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2448,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to referenceWarning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    2,
+    3
   ),
   new Expense(
     "Buy a bicycle",
     8000,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to referenceWarning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    2,
+    8
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2000,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -21,7 +48,7 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    4500,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -30,7 +57,7 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    6000,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -39,16 +66,16 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    3300,
     "tool",
-    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    "Warning: findDOMNode is deprecated in StrictMode.",
     2022,
     2,
     12
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    1800,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -57,7 +84,7 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    7500,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -66,7 +93,7 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    9800,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -75,7 +102,7 @@ export const DUMMY_EXPENSES_DATA = [
   ),
   new Expense(
     "Buy a bicycle",
-    8000,
+    5000,
     "tool",
     "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
     2022,
@@ -90,5 +117,284 @@ export const DUMMY_EXPENSES_DATA = [
     2022,
     2,
     27
+  ),
+  new Expense(
+    "Buy a bicycle",
+    7050,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    4
+  ),
+  new Expense(
+    "Buy a bicycle",
+    8250,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    5
+  ),
+  new Expense(
+    "Buy a bicycle",
+    4090,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    6
+  ),
+  new Expense(
+    "Buy a bicycle",
+    5780,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    7
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2910,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    8
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2250,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    9
+  ),
+  new Expense(
+    "Buy a bicycle",
+    9150,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    10
+  ),
+  new Expense(
+    "Buy a bicycle",
+    4893,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    11
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2050,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    17
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2150,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    18
+  ),
+  new Expense(
+    "Buy a bicycle",
+    5760,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    19
+  ),
+  new Expense(
+    "Buy a bicycle",
+    3491,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    20
+  ),
+  new Expense(
+    "Buy a bicycle",
+    4508,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    21
+  ),
+  new Expense(
+    "Buy a bicycle",
+    7050,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    3
+  ),
+  new Expense(
+    "Buy a bicycle",
+    5050,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    22
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    28
+  ),
+  new Expense(
+    "Buy a bicycle",
+    1550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    3,
+    31
+  ),
+  new Expense(
+    "Buy a bicycle",
+    7550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    1
+  ),
+  new Expense(
+    "Buy a bicycle",
+    5650,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    2
+  ),
+  new Expense(
+    "Buy a bicycle",
+    3941,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    3
+  ),
+  new Expense(
+    "Buy a bicycle",
+    8550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    4
+  ),
+  new Expense(
+    "Buy a bicycle",
+    1550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    10
+  ),
+  new Expense(
+    "Buy a bicycle",
+    9871,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    11
+  ),
+  new Expense(
+    "Buy a bicycle",
+    2550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    12
+  ),
+  new Expense(
+    "Buy a bicycle",
+    12550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    16
+  ),
+  new Expense(
+    "Buy a bicycle",
+    9880,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    17
+  ),
+  new Expense(
+    "Buy a bicycle",
+    6550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    18
+  ),
+  new Expense(
+    "Buy a bicycle",
+    3550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    21
+  ),
+  new Expense(
+    "Buy a bicycle",
+    550,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    22
+  ),
+  new Expense(
+    "Buy a bicycle",
+    1950,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    25
+  ),
+  new Expense(
+    "Buy a bicycle",
+    8750,
+    "tool",
+    "Warning: findDOMNode is deprecated in StrictMode. findDOMNode was passed an instance of Transition which is inside StrictMode. Instead, add a ref directly to the element you want to reference",
+    2022,
+    4,
+    28
   ),
 ];
--- src/data/dummy-tasks.ts
+++ src/data/dummy-tasks.ts
@@ -101,4 +101,214 @@ export const DUMMY_TASK_DATA = [
     7,
     11
   ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "work",
+    1644392000892,
+    2022,
+    2,
+    1
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "learning",
+    1644392700892,
+    2022,
+    2,
+    8
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "friends",
+    1644392000892,
+    2022,
+    2,
+    10
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "1",
+    "friends",
+    1644392502982,
+    2022,
+    1,
+    15
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "2",
+    "family",
+    1644392770892,
+    2022,
+    1,
+    13
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "2",
+    "work",
+    1644393040892,
+    2022,
+    1,
+    6
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "1",
+    "entertainment",
+    1644392850892,
+    2022,
+    1,
+    15
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "3",
+    "entertainment",
+    1644392500892,
+    2022,
+    2,
+    21
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "3",
+    "friends",
+    1644392000892,
+    2022,
+    1,
+    28
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "family",
+    1644395000892,
+    2022,
+    1,
+    21
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "friends",
+    1644392000892,
+    2022,
+    1,
+    7
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "1",
+    "friends",
+    1644392502982,
+    2022,
+    2,
+    15
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "2",
+    "family",
+    1644392770892,
+    2022,
+    2,
+    16
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "2",
+    "work",
+    1644393040892,
+    2022,
+    2,
+    16
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "1",
+    "entertainment",
+    1644392850892,
+    2022,
+    2,
+    19
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "3",
+    "entertainment",
+    1644392500892,
+    2022,
+    1,
+    24
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "3",
+    "friends",
+    1644392000892,
+    2022,
+    2,
+    21
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "family",
+    1644395000892,
+    2022,
+    2,
+    25
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "4",
+    "family",
+    1644395000892,
+    2022,
+    3,
+    8
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "1",
+    "entertainment",
+    1644392850892,
+    2022,
+    3,
+    14
+  ),
+  new Task(
+    "A job interview",
+    "need to get more time to prepare",
+    "3",
+    "entertainment",
+    1644392500892,
+    2022,
+    3,
+    15
+  ),
 ];
